96575cbc0d5129c5dff58fe9887bd13865c17277
JosuneRG <josunerg94@gmail.com>
2025-07-05 11:06:11 +0200

    Eiminar proyecto para crearlo de cero

--- vite-project/src/components_restaurante/Header.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Header() {
-  return (
-    <header style={{ backgroundColor: 'blue', padding: '1rem', color: 'white' }}>
-      <h1>Restaurante React</h1>
-    </header>
-  );
-}
